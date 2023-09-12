e46310a423e8b23dba70c44504ba64e896df559c
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2023-09-12 10:03:01 +0300

    Intro to js

---
+++ intro-to-js.js
@@ -0,0 +1,39 @@
+console.log(3 * (10 - 4));
+
+// Constant expression
+3;
+('Hello');
+
+// Assignment expression
+result = 3 * (10 - 4);
+
+// lookup expression
+result;
+
+/**
+ * Mutability - the value can change
+ * Immutability - the value cannot change
+ */
+// let keyword - mutable
+let age = 20;
+
+age;
+
+age = 21;
+
+console.log(age);
+
+// const keyword - immutable
+const name = 'Nelson';
+
+console.log(name);
+
+// var keyword - never use it
+
+var fullName = 'John Doe';
+
+fullName;
+
+var fullName = 'Jane Doe';
+
+console.log(fullName);
